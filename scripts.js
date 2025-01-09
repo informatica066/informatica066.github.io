f72f9185fcb9b0398e0582760010a708a4841a2d
informatica066 <informa16829@gmail.com>
2025-01-09 15:11:28 +0100

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,4 @@
-// Obtener el botón del menú y el contenedor de enlaces
-const menuIcon = document.getElementById('menu-icon');
-const navLinks = document.getElementById('nav-links');
-
-// Agregar un evento de clic al icono de hamburguesa
-menuIcon.addEventListener('click', function() {
-    // Alternar la clase 'active' en los enlaces del menú
-    navLinks.classList.toggle('active');
+document.getElementById('menu-icon').addEventListener('click', function() {
+    var navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('show');
 });
