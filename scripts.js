b94158a592c1e72599740f2a839ba21ae160afa6
informatica066 <informa16829@gmail.com>
2025-01-09 22:51:55 +0100

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
- document.addEventListener("DOMContentLoaded", function() {
-        const menuIcon = document.querySelector('.menu-icon');
-        const navLinks = document.querySelector('.nav-links');
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const navLinks = document.querySelector('.nav-links');
 
-        // Toggle the 'show' class when the menu icon is clicked
-        menuIcon.addEventListener('click', function() {
-            navLinks.classList.toggle('show');
-        });
+    // Toggle the 'show' class when the menu icon is clicked
+    menuIcon.addEventListener('click', function() {
+        navLinks.classList.toggle('show');
     });
+});
